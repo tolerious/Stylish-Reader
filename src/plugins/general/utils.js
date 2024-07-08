56a6fa3fe0094826262e6c713bcf389cd2d1bb4e
Toly <tolerious.feng@gmail.com>
2024-07-08 21:19:27 +0800

    🚧 Fix special punctuation issue.

--- src/plugins/general/utils.js
+++ src/plugins/general/utils.js
@@ -79,7 +79,18 @@ function convertCurrentTextNodeContent(textNode, targetWordList) {
      * 函数就无法判断出来，这里需要单独处理下这种情况
      *
      * */
-    if (targetWordSet.has(s.toLowerCase())) {
+    console.log(s.toLocaleLowerCase());
+    if (
+      targetWordSet.has(
+        s
+          .toLowerCase()
+          .replace(".", "")
+          .replace(",", "")
+          .replace('"', "")
+          .replace("(", "")
+          .replace(")", "")
+      )
+    ) {
       indexList.push(index);
     }
   });
