2d0b404c151fe67d8cce0e1979c86b39eefb0231
Toly <tolerious.feng@kuka.com>
2024-09-05 17:22:26 +0800

    💄 Update phrase floating panel styles.

--- src/webPage/phraseFloatingPanel/src/views/HomeView.vue
+++ src/webPage/phraseFloatingPanel/src/views/HomeView.vue
@@ -1,5 +1,9 @@
 <template>
-  <div class="bg-slate-300 w-[40px] h-[40px] rounded-full cursor-pointer">12</div>
+  <div
+    class="bg-pink-400 w-[40px] h-[40px] rounded-full cursor-pointer flex justify-center items-center font-bold"
+  >
+    12
+  </div>
 </template>
 
 <script setup lang="ts">
