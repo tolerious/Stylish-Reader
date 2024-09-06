dda3e2de1ffee65f3f404f7b4853190584fb63c1
Toly <tolerious.feng@kuka.com>
2024-09-06 09:56:47 +0800

    🚧 Update phrase floating panel

--- src/webPage/phraseFloatingPanel/src/views/HomeView.vue
+++ src/webPage/phraseFloatingPanel/src/views/HomeView.vue
@@ -1,13 +1,36 @@
 <template>
   <div
-    class="bg-pink-400 w-[40px] h-[40px] rounded-full cursor-pointer flex justify-center items-center font-bold"
+    v-if="isPhraseFloatingIconVisible"
+    class="bg-pink-400 w-[40px] h-[40px] rounded-full cursor-pointer flex justify-center items-center font-bold font-mono select-none"
   >
-    12
+    <span>12</span>
+  </div>
+  <div v-else class="max-h-[300px] h-[300px] overflow-y-scroll p-2 scrollbar select-none">
+    <div v-for="i in 30" :key="i" class="mb-1 border px-2 first-letter:py-1">
+      <div class="grid grid-rows-1 grid-cols-[1fr_30px]">
+        <div>{{ i }}. Get off</div>
+        <div class="cursor-pointer flex justify-center items-start">
+          <span v-if="isLiked">❤️</span> <span v-else>🤍</span>
+        </div>
+      </div>
+      <div>下班下班下班下班下班下班下班下班下班下班</div>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { onMounted } from 'vue'
+import { onMounted, ref } from "vue";
 
-onMounted(() => console.log('home view mounted.'))
+const isPhraseFloatingIconVisible = ref(false);
+
+const isLiked = ref(false);
+
+onMounted(() => console.log("home view mounted."));
 </script>
+
+<style scoped>
+.scrollbar {
+  scrollbar-width: thin;
+  scrollbar-color: #ec4899 #f1f1f1;
+}
+</style>
