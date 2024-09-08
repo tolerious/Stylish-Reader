396e0c09be0352dd628a8a8d40d4ffa91eb7fcf3
Toly <tolerious.feng@gmail.com>
2024-09-08 09:49:43 +0800

    🚧 Fix youtube video issue.

--- src/plugins/youtube/constants.js
+++ src/plugins/youtube/constants.js
@@ -2,6 +2,8 @@ export const youtubeStylishReaderIconId = "youtube-stylish-reader-icon";
 
 export const youtubeVuePageMountPoint = "youtube-stylish-reader-mount-point";
 
+export const youtubeShadowRootId = "youtube-stylish-reader-shadow-root-id";
+
 export const transcriptStatusElementId =
   "youtube-stylish-reader-transcript-status-id";
 
