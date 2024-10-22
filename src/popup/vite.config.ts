283c3c448cb47c1af95a8b419f233b4a278d49de
Toly <tolerious.feng@kuka.com>
2024-10-22 17:15:38 +0800

    🚧 Config to show popup page.

--- src/popup/vite.config.ts
+++ src/popup/vite.config.ts
@@ -4,17 +4,24 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-    vueDevTools(),
-  ],
+  base: './',
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: 'stylish-reader-popup.js', // 入口文件名
+        chunkFileNames: 'stylish-reader-popup-[name].js', // 代码拆分的文件名
+        assetFileNames: 'stylish-reader-popup-[name].[ext]', // 静态资源文件名
+      },
+    },
+  },
+  plugins: [vue(), vueJsx(), vueDevTools(), cssInjectedByJsPlugin()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
 })
