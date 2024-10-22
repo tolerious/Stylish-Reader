a4f070c1b1cb728f5694700df64d63c366a9287e
Toly <tolerious.feng@kuka.com>
2024-10-22 17:21:59 +0800

    🚧 Remove duplicated code.

--- src/popup/src/views/HomeView.vue
+++ src/popup/src/views/HomeView.vue
@@ -1,9 +1,5 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <main>123</main>
 </template>
+
+<script setup lang="ts"></script>
