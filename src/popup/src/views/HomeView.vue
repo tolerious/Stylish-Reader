cebb89adec24bad4c7f0257b8df8ff6b43af1a88
Toly <tolerious.feng@kuka.com>
2024-12-12 15:24:48 +0800

    🎨 Update proto.

--- src/popup/src/views/HomeView.vue
+++ src/popup/src/views/HomeView.vue
@@ -1,5 +1,8 @@
 <template>
-  <main></main>
+  <div class="h-full p-2 grid grid-rows-[50px_1fr] grid-cols-1" container>
+    <div data-title class="text-center">12312</div>
+    <div data-count class="flex justify-around items-center">30个</div>
+  </div>
 </template>
 
 <script setup lang="ts"></script>
