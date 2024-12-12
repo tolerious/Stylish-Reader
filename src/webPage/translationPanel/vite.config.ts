910380d40f2faebbfee5c220b491945e45d63954
Toly <toly.feng@gmail.com>
2024-12-13 07:15:28 +0800

    🎨 Remove unnecessary code.

--- src/webPage/translationPanel/vite.config.ts
+++ src/webPage/translationPanel/vite.config.ts
@@ -1,10 +1,6 @@
 import { fileURLToPath, URL } from "node:url";
 
 import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-import vueJsx from "@vitejs/plugin-vue-jsx";
-import VueDevTools from "vite-plugin-vue-devtools";
-import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
 
 // https://vitejs.dev/config/
 export default defineConfig({
